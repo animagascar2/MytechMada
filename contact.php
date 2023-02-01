581890af247369a6e0af442dc014b58b198561af
animagascar <anima.gascar@gmail.com>
2023-02-01 16:25:59 +0300

    debut

---
+++ contact.php
@@ -0,0 +1,50 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="style.css">
+    <link rel="stylesheet" href="bootstrap-5.0.2-dist/css/bootstrap.css">
+    <link rel="stylesheet" href="fontawesome-free-6.2.1-web/css/all.css">
+    <title>Contact</title>
+</head>
+<body>
+    <div class="containeur" id="ct1">
+        <div class="row" id="ro1">
+            <div class="col-lg-7" id="lg7">
+                <h1>Contactez-nous!</h1>
+                <p>Le monde de demain est déjà arrivé, c’est aujourd’hui. N’attendons pas  demain  si l’on peut agir et interagir maintenant. contactez-nous  directement ou remplissez le formulaire et nous vous répondrons rapidement.</p>
+                <div class="form">
+                    <form action="" method="post">
+                        <div class="nom">
+                            <label for="non">Nom</label>
+                            <input type="text" name="name" id="name">
+                        </div>
+                        <div class="email">
+                            <label for="email">E-mail</label>
+                            <input type="email" name="email" id="email">
+                        </div>
+                        <div class="message">
+                            <label for="message">Message</label>
+                            <textarea name="message" id="message" cols="30" rows="5"></textarea>
+                        </div>
+                        <div class="envoyer">
+                            <input type="submit" value="Envoyer le message">
+                        </div>
+                    </form>
+                </div>
+            </div>
+            <div class="col-lg-5" id="lg5">
+            <img src="contactIMG/Group 11.png" class="img-fluid" alt="...">
+            <div id="p">
+                <p> <i class="fa-solid fa-location-dot"></i>  Antananarivo, Madagascar</p>
+                <p> <i class="fas fa-phone"></i> +261 32 64 372 36</p>
+                <p class="fin"> <i class="fa-solid fa-envelope"></i> noella@mytechmada.com</p>
+            </div>
+            <p class="socio"><i class="fa-brands fa-facebook" id="fb"></i> <i class="fa-brands fa-twitter" id="twit"></i> <i class="fa-brands fa-square-instagram" id="insta"></i></p>
+            </div>
+        </div>
+    </div>
+</body>
+</html>
